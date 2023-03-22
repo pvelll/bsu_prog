5c98189092dd65858794244c8139ed19374ca771
Pavel Sushko <113354603+pvelll@users.noreply.github.com>
2023-03-22 14:35:58 +0300

    Add files via upload

---
+++ oop_1_4B/Source.cpp
@@ -0,0 +1,87 @@
+#include"Header.hpp"
+
+int counter(fstream& lines)
+{
+    int quantity = 0;
+    while (true)
+    {
+        string v;
+        getline(lines, v);
+        if (!lines.eof())
+            quantity++;
+        else
+            break;
+    }
+    int count = quantity / 4;
+    return count;
+}
+
+void insert(Node*& head, Student student) {
+    Node* newNode = new Node;
+    newNode->student = student;
+    newNode->next = nullptr;
+
+    if (head == nullptr) {
+        head = newNode;
+        return;
+    }
+
+    if (student.name < head->student.name) {
+        newNode->next = head;
+        head = newNode;
+        return;
+    }
+
+    Node* current = head;
+    while (current->next != nullptr && current->next->student.name < student.name) {
+        current = current->next;
+    }
+
+    newNode->next = current->next;
+    current->next = newNode;
+}
+
+void printList(Node* head) {
+    while (head != nullptr) {
+        cout << "Name: " << head->student.name << ", Date of birth: " << head->student.dateOfBirth << ", Course: " << head->student.course << ", Marks: " << head->student.gpa << endl;
+        head = head->next;
+    }
+}
+void printList(Node* head, fstream& fout) {
+    cout << "u can see them in the file" << endl;
+    while (head != nullptr) {
+        cout << "Name: " << head->student.name << ", Date of birth: " << head->student.dateOfBirth << ", Course: " << head->student.course << ", Marks: " << head->student.gpa << endl;
+        fout.write(reinterpret_cast <char*>(&head->student.name ), sizeof(char)) ;
+        fout.write(reinterpret_cast <char*>(&head->student.dateOfBirth ), sizeof(char));
+        fout.write(reinterpret_cast <char*>(&head->student.course ), sizeof(int));
+        fout.write(reinterpret_cast <char*>(&head->student.gpa), sizeof(float));
+        head = head->next;
+    }
+}
+
+Node* getStudentsByCourse(Node*& head, int course) {
+    Node* courseHead = nullptr;
+    Node** current = &head;
+
+    while (*current != nullptr) {
+        if ((*current)->student.course == course) {
+            Node* tmp = *current;
+            *current = (*current)->next;
+            tmp->next = nullptr;
+            insert(courseHead, tmp->student);
+        }
+        else {
+            current = &((*current)->next);
+        }
+    }
+
+    return courseHead;
+}
+
+void deleteList(Node*& head) {
+    while (head != nullptr) {
+        Node* tmp = head;
+        head = head->next;
+        delete tmp;
+    }
+}
---
+++ oop_1_4B/oop_1_4B.cpp
@@ -0,0 +1,46 @@
+﻿// oop_1_4B.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include"Header.hpp"
+int main() {
+
+    Node* head = nullptr;
+
+    fstream fin("data.txt",  ios::in);
+    fstream lines("data.txt", ios::in);
+    fstream fout("outData.bin", ios::out, std::ios::binary);
+
+
+    int count = counter(lines);
+    cout << "Number of students: " << count << endl;
+    if (fin.is_open()) {
+        string name;
+        string birtnday;
+        int course;
+        float marks;
+        for (int i = 1; i <= count; i++) {
+            fin >> name;
+            fin >> birtnday;
+            fin >> course;
+            fin >> marks;
+            insert(head, { name, birtnday, course, marks });
+        }
+    }
+
+    cout << "Original List:" << endl;
+    printList(head);
+    int n;
+    cout << "Enter Course" << endl;
+    cin >> n;
+    Node* courseList = getStudentsByCourse(head, n);
+    cout << "Course " << n << " List:" << endl;;
+    printList(courseList, fout);
+    cout << endl;
+    cout << "Remaining List:" << endl;
+    printList(head);
+    deleteList(head);
+    deleteList(courseList);
+    fin.close();
+    fout.close();
+    return 0;
+}
