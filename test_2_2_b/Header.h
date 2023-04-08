b6e84cdcde8cf6889dac70480f57628da57248d9
Pavel Sushko <113354603+pvelll@users.noreply.github.com>
2023-04-08 19:12:50 +0300

    Add files via upload

---
+++ test_2_2_b/Header.h
@@ -0,0 +1,8 @@
+#pragma once
+#include<ctime>
+#include"List.h"
+#include"List.cpp"
+using namespace std;
+void printList(List<int>& , int );
+void filling(List<int>& , List<int>& , int );
+void check_and_fill(List<int>& , List<int>& , List<int>& , int );
