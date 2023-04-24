c4fb203003bc9fceac79721042269d83333a8618
Pavel Sushko <113354603+pvelll@users.noreply.github.com>
2023-04-24 20:24:39 +0300

    oop_3_3_b

---
+++ oop_3_3_B/Header.h
@@ -0,0 +1,11 @@
+#pragma once
+#include<iostream>
+#include<list>
+#include<forward_list>
+enum chioce {
+	qwe = 1,
+	asd
+};
+using namespace std;
+void output_list(list <double> name);
+void sum_res(list <double> list1, list <double> list2);
---
+++ oop_3_3_B/Source.cpp
@@ -0,0 +1,28 @@
+﻿#include "Header.h"
+void output_list(list <double> name)
+{
+	list<double>::iterator p = name.begin();
+	while (p != name.end())
+	{
+		cout << *p << " ";
+		++p;
+	}
+	cout << endl;
+}
+
+void sum_res(list <double> list1, list <double> list2)
+{
+	double elem = 0, elem2 = 0;
+	list <double>::iterator fir = list1.begin();
+	list <double>::reverse_iterator sec = list2.rbegin();
+	double result = 1;
+	while (fir != list1.end())
+	{
+		elem = *sec;
+		elem2 = *fir;
+		result *= elem - elem2;
+		fir++;
+		sec++;
+	}
+	cout << result;
+}
