3c1de6a3e7da003bd70ac51e9b27fed57274d86d
Pavel Sushko <113354603+pvelll@users.noreply.github.com>
2023-06-01 19:37:57 +0300

    Update DynamicArray.cpp

--- Laba_4_full/DynamicArray.cpp
+++ Laba_4_full/DynamicArray.cpp
@@ -1,80 +1,67 @@
 #include "DynamicArray.h"
 
 
-DynamicArray::~DynamicArray()
-{
-    for (int i = 0; i < size; ++i) {
-        delete objects[i];
+DynamicArray::DynamicArray(const DynamicArray& other) : elements(new Base* [other.size]), size(other.size) {
+    for (int i = 0; i < size; i++) {
+        elements[i] = other.elements[i];
     }
-    delete[] objects;
+    realSize = other.realSize;
 }
 
-DynamicArray& DynamicArray::operator=(const DynamicArray& other)
-{
-    if (this != &other) {
-        for (int i = 0; i < size; ++i) {
-            delete objects[i];
-        }
-        delete[] objects;
-        size = other.size;
-        capacity = other.capacity;
-        objects = new Base * [capacity];
-        for (int i = 0; i < size; ++i) {
-            objects[i] = other.objects[i];
-        }
+DynamicArray::~DynamicArray() {
+    for (int i = 0; i < realSize; i++) {
+        delete elements[i];
     }
-    delete &other;
+    delete[] elements;
+}
+
+DynamicArray& DynamicArray::operator=(const DynamicArray& other) {
+    if (this != &other) {
+        delete[] elements;
+        elements = new Base * [other.size];
+        size = other.size;
+        for (int i = 0; i < size; i++) {
+            elements[i] = other.elements[i];
+        }
+        realSize = other.realSize;
+    }
     return *this;
 }
 
-DynamicArray& DynamicArray::operator=(DynamicArray&& other) noexcept
-{
-    if (this != &other) {
-        for (int i = 0; i < size; ++i) {
-            delete objects[i];
-        }
-        size = other.size;
-        capacity = other.capacity;
-        objects = other.objects;
-        other.size = 0;
-        other.capacity = 0;
-        other.objects = nullptr;
+Base& DynamicArray::operator[](int index) {
+    if (index >= realSize) {
+        throw "fuck";
     }
-    delete &other;
-    return *this;
+    return *elements[index];
 }
 
-void DynamicArray::addObject(Base* value)
-{
-    if (size == capacity) {
-        int newCapacity = (capacity == 0) ? 1 : capacity + 2;
-        Base** newData = new Base * [newCapacity];
-        std::copy(objects, objects + size, newData);
-        delete[] objects;
-        objects = newData;
-        capacity = newCapacity;
-    }
-    objects[size] = value;
-    ++size;
+int DynamicArray::getsize() {
+    return realSize;
 }
 
-void DynamicArray::removeObject(int index)
 
-    {
-        if (index >= 0 && index < size) {
-            delete objects[index];
-            for (int i = index; i < size - 1; ++i) {
-                objects[i] = objects[i + 1];
-            }
-            --size;
+void DynamicArray::addObject(Base* element) {
+    if (realSize == size) {
+        Base** newElements = new Base * [size + 1];
+        for (int i = 0; i < realSize; i++) {
+            newElements[i] = elements[i];
         }
+        newElements[realSize] = element;
+        delete[] elements;
+        elements = newElements;
+        size++;
+        realSize++;
+        return;
     }
+    elements[realSize] = element;
+    realSize++;
+}
 
-
-void DynamicArray::printObj() const
-    {
-        for (int i = 0; i < size; ++i) {
-            objects[i]->print();
-        }
-        std::cout << std::endl;
+void DynamicArray::remove(int ind) {
+    delete elements[ind];
+    size--;
+    realSize--;
+    for (int i = ind; i < realSize; i++) {
+        elements[i] = elements[i + 1];
     }
+}
